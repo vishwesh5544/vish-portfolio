25855d030b16c2b7296e585ab1e176206cc50d0c
vishwesh5544 <vishweshshukla20@gmail.com>
2025-04-21 13:52:37 +0530

    update social

--- src/data/social.ts
+++ src/data/social.ts
@@ -2,6 +2,6 @@ import { Social } from "@/types/social";
 
 export const Socials: Social = {
     github: "https://github.com/vishwesh5544",
-    linkedin: "https://www.linkedin.com/in/vishweshs23/",
+    linkedin: "https://www.linkedin.com/in/vishwesh23/",
     instagram: "https://www.instagram.com/vishwesh.io/",
 };
